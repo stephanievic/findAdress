3e9040d5eae5367632c67295f1d460cee2028ca2
Stephanie Victoria <stephanievictoria.dossantos@gmail.com>
2023-01-06 23:19:34 -0300

    adicionando interatividade e API

---
+++ js/script.js
@@ -0,0 +1,58 @@
+const searchBtn = document.querySelector(".search-btn");
+
+//fazer verificações do cep
+function verificaCEP (cep){
+    const cepValido = /^[0-9]{8}$/;
+
+    if (!cepValido.test(cep)){
+        alert("O CEP informado não é válido.");
+    }
+
+    else if (cep == ""){
+        alert("Por favor, informe um CEP.");
+    }
+
+    else getViaCepAPI();
+}
+
+//pegar api
+function getViaCepAPI (){
+    const cep = document.querySelector("#input-cep").value;
+
+    const apiURL = fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    
+    apiURL
+        .then((dados) => {
+            return dados.json();
+        })
+        .then(mostrarEndereco);
+}
+
+//mostrar endereço
+function mostrarEndereco (dados) {
+    let divEndereco = document.querySelector(".result-adress");
+
+    if (dados.erro){
+        divEndereco.innerHTML = "<p><strong>Não foi possivel encontrar este CEP.</strong></p>"
+        divEndereco.style.color = "#3c4e3c";
+    }
+
+    else {
+        divEndereco.innerHTML= `<h2>Endereço encontrado:</h2>
+                                <p><strong>CEP:</strong> ${dados.cep}</p>
+                                <p><strong>Rua: </strong>${dados.logradouro}</p>
+                                <p><strong>Bairro:</strong> ${dados.bairro}</p>
+                                <p><strong>Localidade:</strong> ${dados.localidade} - ${dados.uf}</p>`
+    }
+    
+    document.querySelector("#input-cep").value=("");
+}
+
+//eventos
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    let cep = document.querySelector("#input-cep").value;
+
+    verificaCEP(cep);
+})
